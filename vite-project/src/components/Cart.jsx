71f8a25c38909fcaa3b5d84e199abead7eafba6e
zamanislam <zamni0123@gmail.com>
2025-03-25 06:51:19 +0530

    code pushed

--- vite-project/src/components/Cart.jsx
+++ vite-project/src/components/Cart.jsx
@@ -1,9 +1,29 @@
-const Cart=()=>{
-    return(
-        <div>
-            CartPage
-        </div>
-    )
-}
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default Cart
+const Cart = () => {
+  const location = useLocation();
+  const { cart } = location.state || { cart: [] };
+
+  return (
+    <div>
+      <h1 className='ml-96'>Your Cart</h1>
+      {cart.length === 0 ? (
+        <p>No items in the cart.</p>
+      ) : (
+        <ul className='ml-96'>
+          {cart.map((item, index) => (
+            <li key={index}>
+                <img src={item.image} alt={item.title} width="300" />
+              <h3>{item.title}</h3>
+              <p>Price: ${item.price}</p>
+             
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
--- vite-project/src/components/ProductModal.jsx
+++ vite-project/src/components/ProductModal.jsx
@@ -4,7 +4,7 @@ const ProductModal = ({ product, isOpen, onClose, onAddToCart }) => {
   if (!isOpen || !product) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ml-4">
       <div className="bg-white p-6 rounded shadow-lg">
        
         <img src={product.image} alt={product.title} className="w-32 h-32 mx-auto" />
